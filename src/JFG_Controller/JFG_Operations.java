a08d8b72b33a75edc16cb0d5c3a2cb80059b2ae6
jhoda <jhoda@LAPTOP-AF0JKBQG>
2020-11-22 12:37:09 -0500

    Version 1

---
+++ src/JFG_Controller/JFG_Operations.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JFG_Controller;
+
+/**
+ *
+ * @author jhoda
+ */
+public class JFG_Operations {
+    
+}
